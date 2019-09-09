abab25fbe1401e76c7eb341f20d15052f1371e52
KonradMarzec1991 <konrimarzec@gmail.com>
2019-09-09 22:44:41 +0200

    MovieList

--- mymdb/mymdb/settings.py
+++ mymdb/mymdb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mymdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': 'templates',
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
