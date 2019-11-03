d77339a4c37a3d7ae21b6d28bd9644ce15130f10
KonradMarzec1991 <konrimarzec@gmail.com>
2019-11-03 21:13:57 +0100

    Create dockerfile and bash files

---
+++ env/lib/python3.7/_collections_abc.py
@@ -0,0 +1,1 @@
+/home/konrad/anaconda3/lib/python3.7/_collections_abc.py
---
+++ scripts/collect_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+root=$1
+source $root/venv/bin/activate
+
+export DJANGO_CACHE_TIMEOUT=100
+export DJANGO_SECRET_KEY=FAKE_KEY
+export DJANGO_SETTINGS_MODULE=mymdb.production_settings
+export DJANGO_LOG_FILE=/var/log/mymdb/mymdb.log
+cd $root/django/
+
+python manage.py collectstatic
+
